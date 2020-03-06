b9d19df8117e4ed46747fed3d7d8c2ef2b378d2f
Thembela <mbelaselani@gmail.com>
2020-03-06 15:00:37 +0200

    2nd commit

--- src/simple_calculator.js
+++ src/simple_calculator.js
@@ -1,20 +1,34 @@
-function add() {
+//Add function
+function add(num1, num2) {
+  return num1 + num2
+}
+
+function add() {           
+
   let sum = 0;
-  for (i = 0; i < arguments.length; i++) {
-    sum += arguments[i];
+  for (i = 0; i < arguments.length; i++) {  
+      sum += arguments[i];
   }
+
   return sum;
+};
+
+
+//Multiply function
+function multiply(num1, num2) {
+  return num1 * num2
 }
 
 function multiply() {
-  let product = 1;
-  for (i = 0; i < arguments.length; i++) {
-    product *= arguments[i];
-  }
-  return product;
-}
-
-module.exports = {
-  add,
-  multiply
-};
+  
+      let product = 1;
+      for (i = 0; i < arguments.length; i++) {
+          product *= arguments[i];
+      }
+  
+      return product;
+  };
+  
+  console.log(add, multiply);         
+  
+  module.exports = { add, multiply }       
